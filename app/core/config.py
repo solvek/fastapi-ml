5479134b58591e0a6729b1972dba77e7b3b00fcd
solvek <adamchuk@gmail.com>
2024-04-23 10:26:59 +0300

    Rename project

--- app/core/config.py
+++ app/core/config.py
@@ -2,7 +2,7 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    PROJECT_NAME: str = "FastAPI Template"
+    PROJECT_NAME: str = "SolvekNLP server"
     API_V1_STR: str = "/api/v1"
 
     MODEL_PATH: str = "app/ml_model/model.pth"
