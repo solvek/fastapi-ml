3989b6853ad42bd6678f0f8a42b51fa6fa4c749d
solvek <adamchuk@gmail.com>
2024-04-26 13:30:36 +0300

    Some more transformations

--- app/api/transform.py
+++ app/api/transform.py
@@ -6,6 +6,8 @@ from sklearn.pipeline import make_pipeline
 import nltk
 
 import re
+from collections import Counter
+from nltk.corpus import stopwords
 
 transform_router = APIRouter()
 
@@ -17,6 +19,7 @@ nltk.download('words')
 nltk.download('wordnet', quiet=True)
 nltk.download('omw-1.4', quiet=True)
 nltk.download('vader_lexicon', quiet=True)
+nltk.download('stopwords')
 
 
 class StandardizeLetters:
@@ -45,9 +48,42 @@ class RemoveNumbers:
         return re.sub(r'\d', '', text)
 
 
+class RemoveRareWords:
+    def __init__(self, threshold=5):
+        # print("Threshold is", threshold)
+        self.threshold = threshold
+
+    def transform(self, text):
+        words = nltk.word_tokenize(text)
+        word_freq = Counter(words)
+
+        filtered_words = [word for word in words if word_freq[word] >= self.threshold]
+
+        return ' '.join(filtered_words)
+
+
+class Tokenize:
+    @staticmethod
+    def transform(text):
+        return nltk.word_tokenize(text)
+
+
+class FilterStopWords:
+    def __init__(self, language="english"):
+        # print("Threshold is", threshold)
+        self.stop_words = set(stopwords.words(language))
+
+    def transform(self, tokens):
+        return [token for token in tokens if token not in self.stop_words]
+
+
 def create_transformer(step):
     klass = globals()[step.transformer]
-    return klass()
+    p = step.params
+    if p is None:
+        return klass()
+
+    return klass(**step.params)
 
 
 @transform_router.post("/transform")
